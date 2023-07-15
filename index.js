c1775a427d1d044e121a10a9de133faf5eff30b3
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2023-07-15 18:57:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ const __dirname = dirname(__filename);
 
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
+
 app.use(cors());
 dotenv.config();
 startCronJobs();
