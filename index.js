3d79395978fbe4844e10a0d865a06aa81b587adb
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2023-07-15 22:04:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,6 @@ const __dirname = dirname(__filename);
 
 
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 app.use(cors(
     {
