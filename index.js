a6fa5a1164cad444631ebd1d266b88d4b501001b
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2023-07-16 12:36:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,18 +23,12 @@ const app = express();
 
 app.use(cors(
  {
-  origin : ['https://stackoverflowclone-three.vercel.app/'],
+  origin : ['https://stackoverflowclone-three.vercel.app'],
   methods : ['GET','POST','PUT','DELETE'],
   credential : true
  }
 ));
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+
 
 dotenv.config();
 startCronJobs();
