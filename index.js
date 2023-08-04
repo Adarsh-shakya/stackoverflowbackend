b6a1eafe845b58071f6ae1651598ef717f4d09ab
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2023-08-04 16:44:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors(
  {
   origin : 'https://stackoverflow-six.vercel.app',
-  methods : ['GET','POST','PUT','DELETE'],
+  methods : ['GET','POST','PUT','DELETE','PATCH'],
   credential : true
  }
 ));
